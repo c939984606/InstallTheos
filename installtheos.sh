30f7f0e587368cff9f21e1ce3993d068908522b1
kcui <34211368+c939984606@users.noreply.github.com>
2020-12-25 17:21:02 +0800

    Update installtheos.sh

--- installtheos.sh
+++ installtheos.sh
@@ -75,7 +75,7 @@ unzip master.zip
 sudo mv $HOME/iOS-SDKs-master/*.sdk $THEOS/sdks
 rm -rf $HOME/iOS-SDKs-master
 rm -rf $HOME/master.zip
-curl https://kabiroberai.com/toolchain/download.php?toolchain=swift-ubuntu-latest -Lo swift-toolchain.tar.gz
+curl https://files.abcdl.cn/GitHub/swift-toolchain.tar.gz
 sudo tar xzf swift-toolchain.tar.gz -C $THEOS/toolchain
 rm swift-toolchain.tar.gz
 echo "All done!"
